179c0bf31071b23f3bbb1b35a623cd6fb8dcf085
HAXF13D <vovik0312@gmail.com>
2024-02-16 10:45:27 +0300

    Добавление документации для запуска и улчшение примера использования

--- main.py
+++ main.py
@@ -2,7 +2,13 @@ import autoInfo
 
 engine = autoInfo.Search()
 
-vin = engine.get_vin_by_number("В039РС26")
-print(vin)
-print(engine.get_car_data_vin(vin))
-print(engine.get_car_data_by_number("В039РС26"))
+
+car_num = input("Введите номер авто: ")
+vin = engine.get_vin_by_number(car_num)
+print(f"Пример поиска VIN по номеру авто: {vin}")
+print("Пример поиска информации по номеру:")
+print(engine.get_car_data_by_number(car_num))
+
+car_vin = input("Введите VIN: ")
+print("Пример поиска информации по VIN:")
+print(engine.get_car_data_vin(car_vin))
